ade3ffdf37d1310298571b6018648cecc0677467
ryanchou-dev <ryanchou111@gmail.com>
2023-02-08 02:58:01 -0800

    uwi

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,11 +15,40 @@ export default function Home({ ext }: Props) {
   return (
     <>
       <div
+        className="md:block hidden absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none"
+        aria-hidden="true"
+      >
+        <svg
+          width="1360"
+          height="578"
+          viewBox="0 0 1360 578"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <linearGradient
+              x1="50%"
+              y1="0%"
+              x2="50%"
+              y2="100%"
+              id="illustration-01"
+            >
+              <stop stopColor="#cbb0e8" offset="0%" />
+              <stop stopColor="#dbcfe8" offset="77.402%" />
+              <stop stopColor="#DFDFDF" offset="100%" />
+            </linearGradient>
+          </defs>
+          <g fill="url(#illustration-01)" fillRule="evenodd">
+            <circle cx="1232" cy="128" r="128" />
+            <circle cx="155" cy="443" r="64" />
+          </g>
+        </svg>
+      </div>
+      <div
         className={`md:py-24 md:px-36 px-24 py-16 min-h-screen bg-slate-200`}
       >
         {/* <p className=" text-6xl text-opzacity-20">uwi</p>t */}
         <div className="text-center align-center text-black text-6xl">🌃</div>
-        <div className="flex justify-center align-center">
+        <div className="flex justify-center align-center mt-">
           <p className="font-bold text-6xl p-2 rounded-lg  bg-purple-400 bg-opacity-10 w-fit">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-purple-700">
               Art
@@ -31,12 +60,17 @@ export default function Home({ ext }: Props) {
           The place for trading art pieces.
         </p>{" "}
         <div className="flex justify-center align-center">
-          <button
-            onClick={() => signIn("google")}
-            className={`hover:scale-105 duration-300 mt-6 space-x-2 flex border-purple-300 items-center font-md w-max p-2.5 border-2 rounded-lg`}
-          >
-            <FcGoogle className={`w-6 h-6`} /> <span>Sign in with Google</span>
-          </button>
+          {!ext ? (
+            <button
+              onClick={() => signIn("google")}
+              className={`hover:scale-105 duration-300 mt-6 space-x-2 flex items-center font-md w-max p-2.5 border-2 rounded-lg border-purple-400`}
+            >
+              <FcGoogle className={`w-6 h-6`} />{" "}
+              <span>Sign in with Google</span>
+            </button>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </>
