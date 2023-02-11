969b284284bf3f957c8c066a81781f53033010bd
Maggie Liu <maggie.j.liu@gmail.com>
2023-02-11 12:16:44 -0800

    navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import Navbar from "@/components/Navbar";
 
 export default function App({
   Component,
@@ -8,6 +9,7 @@ export default function App({
 }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Navbar />
       <Component {...pageProps} />
     </SessionProvider>
   );
