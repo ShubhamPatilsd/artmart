22788d3e4598270067e84f7177e9d874f47d6953
Maggie Liu <maggie.j.liu@gmail.com>
2023-02-11 11:40:15 -0800

    log in works

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,19 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { data: session } = useSession();
+  if (session) {
+    return <>{session.user!.email}</>;
+  }
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button onClick={() => signIn("google")}>Sign in with Google</button>
     </>
   );
 }
