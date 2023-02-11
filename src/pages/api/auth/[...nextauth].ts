4c57124f6c457cade9e07230e7c64195636930c2
Shubham <shubhampatilsd@gmail.com>
2023-02-11 11:06:23 -0800

    nextauth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import AppleProvider from "next-auth/providers/apple";
+import FacebookProvider from "next-auth/providers/facebook";
+import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+});
