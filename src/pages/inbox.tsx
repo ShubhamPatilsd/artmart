234e6e1c0508a98058fa37aaa69cc425308c6e30
Shubham <shubhampatilsd@gmail.com>
2023-02-11 18:08:40 -0800

    inbox

---
+++ src/pages/inbox.tsx
@@ -0,0 +1,93 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { FcGoogle } from "react-icons/fc";
+import { db } from "@/db/db";
+import { Post as PostCard } from "@/components/Post";
+import { useRouter } from "next/router";
+import { Post } from "../types/Post";
+import { getServerSession } from "next-auth";
+import { NextApiRequest, NextApiResponse } from "next";
+import { authOptions } from "./api/auth/[...nextauth]";
+import truncate from "truncate";
+import useCollapse from "react-collapsed";
+import { VscTriangleRight } from "react-icons/vsc";
+
+const dummyData = [
+  {
+    subject: "New trade request for Mono Liso from Gautam Paranjape",
+  },
+  {
+    subject: "New trade request for Mono Liso from Gautam Paranjape",
+  },
+  {
+    subject: "New trade request for Mono Liso from Gautam Paranjape",
+  },
+  {
+    subject: "New trade request for Mono Liso from Gautam Paranjape",
+  },
+  {
+    subject: "New trade request for Mono Liso from Gautam Paranjape",
+  },
+  {
+    subject: "New trade request for Mono Liso from Gautam Paranjape",
+  },
+  {
+    subject: "New trade request for Mono Liso from Gautam Paranjape",
+  },
+];
+
+export default function Inbox({ posts }: { posts: Post[] }) {
+  const router = useRouter();
+
+  return (
+    <div className="flex justify-center">
+      <div className="w-[75%]">
+        {dummyData.map((post, i) => {
+          const { getCollapseProps, getToggleProps, isExpanded } =
+            useCollapse();
+          return (
+            <div className="border-t hover:shadow-md relative px-6 py-2  justify-between cursor-pointer border-purple-300 shadow-purple-200">
+              <p className="font-medium  text-gray-600 text-sm text-left">
+                {post.subject}
+              </p>
+              <button
+                className="mt-2 flex space-x-1 items-center"
+                {...getToggleProps()}
+              >
+                {" "}
+                <VscTriangleRight
+                  className={`${isExpanded && "rotate-90"} text-gray-600`}
+                  size={12}
+                />
+                <p className="text-xs text-gray-600">Expand</p>
+              </button>
+
+              {isExpanded}
+              <p {...getCollapseProps()}>hi</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export const getServerSideProps = async (context: any) => {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  const requests = await db.trade.findMany({
+    where: {
+      authorId: session.user?.email,
+    },
+    include: {
+      to: true,
+      from: true,
+      postInQuestion: true,
+    },
+  });
+  return { props: { posts: JSON.parse(JSON.stringify(requests)) } };
+};
