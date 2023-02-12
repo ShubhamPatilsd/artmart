598c26387b15cccb0c1fe49f87ad3eda555fb860
Shubham <shubhampatilsd@gmail.com>
2023-02-11 19:21:58 -0800

    tile fix

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -18,18 +18,18 @@ export const Post: React.FC<PostProps> = ({
   return (
     <div
       className={
-        "group hover:bg-blue-900 hover:bg-opacity-40 hover:border-transparent hover:shadow-lg duration-100 rounded-md border border-gray-300 p-4"
+        "group relative hover:bg-opacity-40 hover:border-transparent hover:shadow-lg duration-100 rounded-md border border-gray-300"
       }
     >
       {/* TODO: change this to nextjs image */}
-      <div className="h-30 md:h-36 lg:h-48 rounded-lg flex justify-center">
+      <div className="h-30 md:h-36 lg:h-48 rounded-lg">
         <img
           src={coverPhoto}
-          className="shadow-lg relative z-10 group-hover:brightness-50 h-full w-auto  rounded-t-lg"
+          className="shadow-lg relative z-10 group-hover:brightness-50 h-full w-full object-cover rounded-lg"
         />
       </div>
-      <div>
-        <div className="bg-indigo-500 bg-opacity-80 h-auto absolute opacity-0 group-hover:opacity-100 z-50 -mt-24 p-2 rounded-lg  pr-8 w-auto">
+      <div className="">
+        <div className="bg-indigo-500 bg-opacity-80 h-auto absolute opacity-0 transition duration-75 group-hover:opacity-100 z-50 bottom-3 left-3 p-2 rounded-lg  pr-8 w-auto">
           <span className="bg-purple-100 px-2 py-1 text-purple-700 font-medium tracking-wider lowercase rounded-md text-xs">
             {category}
           </span>
